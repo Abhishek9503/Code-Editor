f0f1b70d0ea36f6265f54f946696ff6a90c80008
Abhishek <abhishek.pub9503@gmail.com>
2024-12-17 01:00:36 +0530

    feat: Added output Panel

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -11,14 +11,14 @@ export default function Home() {
     <div className='min-h-screen'>
 
       <div className='max-w-[1800px] mx-auto p-4 '>
-        <Header/>
+        <Header />
 
         <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
           <EditorPanel/>
           <OutputPanel/>
         </div>
       </div>
-    
+
     </div>
   );
 }
